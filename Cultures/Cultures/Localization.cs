dda0e079be1594bd964d47102eaa9e06b183e994
red-the-random-dev <78296390+red-the-random-dev@users.noreply.github.com>
2021-02-02 18:59:11 +1000

    Localization class hotfix

---
+++ Cultures/Cultures/Localization.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * Localization class.
+ */
+using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace DotGraphics.Cultures
+{
+	/// <summary>
+	/// Container for localization lines.
+	/// </summary>
+	public static partial class Localization
+	{
+		static Dictionary<String, Localization.D_Receive> LanguageGetters = new Dictionary<string, Localization.D_Receive>();
+		static List<String> Languages = new List<String>();
+		
+		static Boolean Ready = false;
+		public delegate String D_Receive(String Key);
+		
+		/// <summary>
+		/// Method to be run in entry point.
+		/// </summary>
+		public static void Initialize()
+		{
+			InitializeEnglish();
+			InitializeRussian();
+			
+			Ready = true;
+		}
+		
+		static Boolean HasLang(String Lang)
+		{
+			foreach (String i in Languages)
+			{
+				if (Lang == i)
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+		
+		/// <summary>
+		/// Return one of supported keys.
+		/// </summary>
+		/// <param name="Key">String that corresponds with needed key.</param>
+		/// <returns>Language-dependent text line.</returns>
+		public static String Get(String Key)
+		{
+			if (!Ready)
+			{
+				Initialize();
+			}
+			
+			String Lang = CultureInfo.CurrentCulture.ToString();
+			return LanguageGetters[(HasLang(Lang) ? Lang : "en-US")](Key);
+		}
+	}
+}
---
+++ Cultures/Cultures/Localization_Russian.cs
@@ -0,0 +1,65 @@
+﻿/*
+ * Russian localization.
+ */
+using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace DotGraphics.Cultures
+{
+	public static partial class Localization
+	{
+		static Dictionary<String, String> Russian = new Dictionary<string, string>();
+		
+		static void InitializeRussian()
+		{
+			// Window titles
+			Russian.Add("wintitle", "DotGraphics Braille Renderer");
+			Russian.Add("alerttitle", "Braille Renderer - Сообщение");
+			// Success
+			Russian.Add("ok_render", "Рендер текста завершён!");
+			Russian.Add("ok_compiled", "Сборка холста завершена!");
+			// Errors
+			Russian.Add("error_pngnotfound", "Не удалось найти изображение по данному запросу.");
+			Russian.Add("error_objectnotfound", "Не удалось найти объет по этому расположению: ");
+			Russian.Add("error_renderdumpfail", "Нам не удалось записать объект в файл.");
+			Russian.Add("error_objectloadfail", "Ёк-макарёк! С файлом, похоже, что-то не так!");
+			Russian.Add("error_imageaspectratio", "Изображение не соответствует установленным параметрам: ширина не делится на 2 & высота не делится на 4.");
+			Russian.Add("error_imagetoolarge", "Размер изображения слишком велик: ширина и высота должны быть меньше 65536 точек.");
+			Russian.Add("error_renderfail", "Произошёл сбой во время разгрузки изображения в файл.");
+			// Section titles
+			Russian.Add("ui_complabel", "Собрать холст Брайля из PNG-изображения:");
+			Russian.Add("ui_renderlabel", "Перенести содержимое холста в текст:");
+			// Compile section
+			Russian.Add("ui_pnglabel", "Расположение PNG-файла...");
+			Russian.Add("ui_objlabel", "Путь сохранения объекта...");
+			Russian.Add("ui_compilebutton", "Собрать");
+			// Render section
+			Russian.Add("ui_targetlabel", "Расположение согбранного холста...");
+			Russian.Add("ui_txtlabel", "Путь сохранения текстового файла...");
+			Russian.Add("ui_renderbutton", "Рендер");
+			// Save, Load, and Show result
+			Russian.Add("ui_loadbutton", "Открыть...");
+			Russian.Add("ui_savebutton", "Сохранить...");
+			Russian.Add("ui_showresult", "Показать результат после рендеринга изображения");
+			// File type filters
+			Russian.Add("filter_txt", "Текстовый документ (*.txt)|*.txt");
+			Russian.Add("filter_png", "PNG-изображение (*.png)|*.png");
+			Russian.Add("filter_o", "Сериализованный объект (*.o)|*.o");
+			
+			LanguageGetters.Add("ru-RU", GetRussian);
+			Languages.Add("ru-RU");
+		}
+		
+		static String GetRussian(String Key)
+		{
+			if (!Ready)
+			{
+				Initialize();
+				return GetRussian(Key);
+			}
+			
+			return Russian[Key];
+		}
+	}
+}
